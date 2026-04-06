01b50b332224aac5c63bf1caee602469aef6571b
dev-xero <ssh.xero@gmail.com>
2026-04-06 14:21:31 +0100

    feat: add http swagger autogen

--- backend/internal/handlers/root_handler.go
+++ backend/internal/handlers/root_handler.go
@@ -9,6 +9,19 @@ import (
 
 type RootHandler struct{}
 
+// RootHandler handles requests to the root index
+//
+// @Summary Handles base requests
+// @Tags root
+// @Accept json
+// @Produce json
+// @Success 200
+// @Router /api/v1 [get]
 func (h *RootHandler) Get(c fiber.Ctx) error {
-	return utils.Success(c, http.StatusOK, "API v1.0 is online", "visit the swagger specification with /schema/swagger")
+	return utils.Success(
+		c,
+		http.StatusOK,
+		"API v1.0 is online",
+		"visit the swagger specification with /schema/swagger/index.html",
+	)
 }
