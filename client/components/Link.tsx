0f0511e613b3250a40515428a86da159de29b5dc
dev-xero <ssh.xero@gmail.com>
2026-04-08 17:43:18 +0100

    chore: remove selection from link elements

--- client/components/Link.tsx
+++ client/components/Link.tsx
@@ -8,7 +8,7 @@ export default function Link(props: ILinkProps) {
   return (
     <a
       href={props.href}
-      className="text-sm text-faded hover:underline underline-offset-4 hover:text-foreground"
+      className="select-none text-sm text-faded hover:underline underline-offset-4 hover:text-foreground"
       target={props.external ? "_blank" : "_self"}
     >
       {props.label}
